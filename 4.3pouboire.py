f8f4f020d33cac2e48132aab7951481700006da9
2259686 <2259686@cstjean.qc.ca>
2022-09-27 10:50:17 -0400

    Exercise 4.3 pourboire.

--- 4.3pouboire.py
+++ 4.3pouboire.py
@@ -1,19 +1,45 @@
-# Auteur : Antoine
+# Auteur : Fait par Antoine
+# Date de création : 27 septembre 2022
 
-# CONSTANTES
+# Algorithme :
+# Ce programme servira à déterminer s'il y a un pourboire à donner et
+# quel serait le montant recommandé tout donnant le montant total de la facture.
 
+import math
 
 # VARIABLES
-prix_pourboire: float
-prix_facture: float
-
+prix_pourboire: float  # valeur du pourboire recommandé
+prix_facture: float  # prix de la facture demandé en input
 
 # LOGIQUE
-print("Quelle est le montant de votre facture?")
-prix_facture = float(input("Montant de la facture:"))
+while True:
+    try:
+        print("Quelle est le montant de votre facture?")
+        prix_facture = float(input("Montant de la facture:"))
+        if prix_facture < math.inf:
+            break
+    except:
+        print("Pas de lettres dans le montant!")
+        quit()
+
+if prix_facture <= 10:
+    prix_pourboire = prix_facture + 1.50
+
+elif prix_facture < 100:
+    prix_pourboire = prix_facture * 0.15
+
+elif prix_facture < 200 or prix_facture == 100:
+    prix_pourboire = (prix_facture * 0.05) + 15
+
+else:
+    prix_pourboire = 0
+
+if 200 > prix_facture > 0:
+    print(f"Votre montant total de votre facture est de {(prix_facture + prix_pourboire) :.2f} $ \n"
+          f"et la valeur du pourboire est de {prix_pourboire:.2f} $.")
+
+if prix_facture >= 200:
+    print("La valeur du pourboire est de 0$ car celui-ci est inclu.")
 
 if prix_facture < 0:
-    print("Pas besoin de pourboire, il vous doit de l'argent!")
-elif prix_facture < 10:
-    print("Un pourboire de 1,50$ est recommandé.")
-elif prix_facture < 100:
+    print("Impossible, la facture ne peut pas être négative.")
